6ceb9b6b6a4986a6b1ca3be20280500097dae6a6
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 20:58:14 +0300

    feat: add IUserService

---
+++ AuthServer.Core/DTOs/UserAppDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class UserAppDto
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<ResponseDto<UserAppDto>> GetUserByUserName(string userName);
+    }
+}
