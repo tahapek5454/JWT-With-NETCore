90a4cce524f5768f287f08e98ca24faf8a857c34
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 18:38:59 +0300

    feat: add models

---
+++ AuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public int id { get; set; }
+        public int UserId { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
