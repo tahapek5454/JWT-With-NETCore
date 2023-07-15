e17bc7a932ce22401b1fbeaeee3d58344891a402
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 18:59:49 +0300

    feat: add ErrorDto for shared

---
+++ SharedLibrary/DTOs/ErrorDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.DTOs
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; }
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors= new List<string>();
+        }
+        public ErrorDto(string error, bool isShow) : this()
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool ısShow)
+        {
+            Errors = errors;
+            IsShow = ısShow;
+        }
+
+        
+    }
+}
