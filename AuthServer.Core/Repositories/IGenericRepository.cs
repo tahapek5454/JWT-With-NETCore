074b2a91fabf8a55b95d83c84fcd76730e36a2be
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 18:51:12 +0300

    feat: add IGenericRepo

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<T>
+        where T : class
+    {
+        DbSet<T> Table { get; }
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        T Update(T entity);
+    }
+}
