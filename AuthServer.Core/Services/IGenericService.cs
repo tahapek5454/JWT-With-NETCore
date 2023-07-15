a4311500b28060d70a8034bca5e312f991f1694c
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 19:38:06 +0300

    feat: add genericService

---
+++ AuthServer.Core/Services/IGenericService.cs
@@ -0,0 +1,20 @@
+﻿using SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IGenericService<TEntiy, TDto>
+        where TDto : class
+        where TEntiy: class
+    {
+        Task<ResponseDto<TDto>> GetByIdAsync(int id);
+        Task<ResponseDto<List<TDto>>> GetAllAsync();
+        Task<ResponseDto<TDto>> AddAsync(TEntiy entiy);
+        ResponseDto<TDto> Remove(TEntiy entiy);
+        ResponseDto<TDto> Update(TEntiy entiy);
+    }
+}
