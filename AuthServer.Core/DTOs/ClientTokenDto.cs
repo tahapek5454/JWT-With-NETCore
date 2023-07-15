b810a4eb92e30a12163ae4267c67f0ea4ad8a1f9
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 20:14:52 +0300

    feat: add token dtos

---
+++ AuthServer.Core/DTOs/ClientTokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class ClientTokenDto
+    {
+        // just access token for selected client not for identity
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
---
+++ AuthServer.Core/DTOs/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class TokenDto
+    {
+        //access and refresh for classic arch identity
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
