4c6977f11b95bdae1197b7eabc8333a5a9747649
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 20:48:56 +0300

    feat: add IAuthentication service

---
+++ AuthServer.Core/DTOs/ClientLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.DTOs
+{
+    public class ClientLoginDto
+    {
+        public string ClintId{ get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        // we don't return data just fit format
+        Task<ResponseDto<TokenDto>> RevokeRefreshToken(string refreshToken);
+
+        // not for subscribe process just let for access specific clients
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
