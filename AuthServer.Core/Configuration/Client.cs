08e5dd296c02cc0325183ccf412455671569f4f6
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 20:32:06 +0300

    feat: add ITokenService

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        // oAuth 2.0 
+        public string Id { get; set; }
+        public string Secret { get; set; }
+
+        // detecting which server the client can access 
+        //  no sub no identity for ex www.myapi3.com
+        public List<string> Audiences { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        // this just for selected client doesn't exist identity
+        ClientTokenDto CreateTokenByClient(Client client);
+
+    }
+}
