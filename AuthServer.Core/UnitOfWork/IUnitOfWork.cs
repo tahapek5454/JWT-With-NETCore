39442162a44d2e55855912a8b3ba412643650a41
tahapek5454 <tahapek5454@gmail.com>
2023-07-15 21:05:09 +0300

    feat: add IUnitOfWork

---
+++ AuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        // about for saveChanges, thats design pattern include transaction
+
+        Task CommitAsync();
+        void Commit();
+    }
+}
