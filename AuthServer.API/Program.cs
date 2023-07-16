a3b1117b44910e4a4788eb3ffa1c2582d389f8e4
tahapek5454 <tahapek5454@gmail.com>
2023-07-16 14:14:43 +0300

    feat: add service registrations

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -1,3 +1,5 @@
+using AuthServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDataService(builder.Configuration);
 
 var app = builder.Build();
 
--- AuthServer.Data/ServiceRegistration.cs
+++ AuthServer.Data/ServiceRegistration.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using AuthServer.Core.Models;
+using AuthServer.Data.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +14,18 @@ namespace AuthServer.Data
 {
     public static class ServiceRegistration
     {
-        public static void AddDataService(this IServiceCollection serviceCollection)
+        public static void AddDataService(this IServiceCollection serviceCollection, IConfiguration configuration)
         {
+            serviceCollection.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration["ConnectionStrings:PostgreSQL"]));
+
+            serviceCollection.AddIdentity<UserApp, IdentityRole<int>>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
 
         }
     }
