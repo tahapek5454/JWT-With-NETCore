0e759626a7c1c69ad40cde0eea50bf9d12a85851
tahapek5454 <tahapek5454@gmail.com>
2023-07-16 13:06:12 +0300

    feat: add unitOfWork

---
+++ AuthServer.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using AuthServer.Core.UnitOfWork;
+using AuthServer.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Commit()
+            => _appDbContext.SaveChanges();
+
+        public async Task CommitAsync()
+            => await _appDbContext.SaveChangesAsync();
+    }
+}
