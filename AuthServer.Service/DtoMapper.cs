a5d88358dcd3ac97684508e729229fffbca10523
tahapek5454 <tahapek5454@gmail.com>
2023-07-16 14:31:30 +0300

    feat: add auto mapper

---
+++ AuthServer.Service/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    internal class DtoMapper: Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
---
+++ AuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        // lazy loading for ram capacity. Just needed create
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper { get { return lazy.Value; } }
+    }
+}
