a499d5ef2b8b22d7891249fccb014458c615c913
tahapek5454 <tahapek5454@gmail.com>
2023-07-17 23:04:39 +0300

    feat: add SignService

---
+++ AuthServer.Service/Services/SignService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public static class SignService
+    {
+        // asimetrik key for token
+        // auth server sign token with private key and api resolve token with public key
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+            => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+    }
+}
