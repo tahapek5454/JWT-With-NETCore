8e82764fb55a17233968318b1450d475d9798f6b
tahapek5454 <tahapek5454@gmail.com>
2023-07-18 18:53:07 +0300

    feat: add createRefreshToken on TokenService

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,52 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOptions _customTokenOptions;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOptions> options)
+        {
+            _userManager = userManager;
+            _customTokenOptions = options.Value;
+     
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientTokenDto CreateTokenByClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CreateRefreshToken()
+        {
+            //32 byte random string data
+
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+
+        }
+    }
+}
