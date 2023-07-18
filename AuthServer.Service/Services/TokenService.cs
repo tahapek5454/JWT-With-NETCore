479d0fbecd3d2c15b055ec6f6c8e51c0997270ed
tahapek5454 <tahapek5454@gmail.com>
2023-07-18 19:18:09 +0300

    feat: add GetClaims method for TokenService

--- AuthServer.Service/Services/TokenService.cs
+++ AuthServer.Service/Services/TokenService.cs
@@ -7,7 +7,9 @@ using Microsoft.Extensions.Options;
 using SharedLibrary.Configurations;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,6 +49,30 @@ namespace AuthServer.Service.Services
 
             return Convert.ToBase64String(numberByte);
 
+        }
+
+        private IEnumerable<Claim> GetClaims(UserApp userApp, List<string> audiences) 
+        {
+            // token's payload is claims
+
+            // if you want you can write "id","1" - "email","example.com" ...
+            // but if you want the claims to pair with identity lib you can use const type 
+            // For example you can use in controller User.Identity.Name it pair to ClaimTypes.Name
+            // otherwise you must User.claims(c => c.type == "myUserName")
+            var userClaimList = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, userApp.Id.ToString()),
+                new Claim(ClaimTypes.Email, userApp.Email),
+                new Claim(ClaimTypes.Name, userApp.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            // the last one for like a pk
+            // this claims about user, after created jwt they added to payload
+
+            userClaimList.AddRange(audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+            // we apply to format to arch can find Audiencess 
+        
+            return userClaimList;
         }
     }
 }
