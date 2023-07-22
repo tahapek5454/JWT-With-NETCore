d8cf430c58a4f9239b0a8b340040a696f2d676f4
tahapek5454 <tahapek5454@gmail.com>
2023-07-22 14:12:34 +0300

    feat: add example product controller

---
+++ AuthServer.API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IGenericService<Product, ProductDto> _genericService;
+
+        public ProductsController(IGenericService<Product, ProductDto> genericService)
+        {
+            _genericService = genericService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var result = _genericService.GetAllAsync();
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductDto productDto)
+        {
+            var result = await _genericService.AddAsync(productDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct(ProductDto productDto)
+        {
+            var result = _genericService.Update(productDto);
+            return ActionResultInstance(result);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductAsync(int id)
+        {
+            var result = await _genericService.Remove(id);
+            return ActionResultInstance(result);
+        }
+
+
+    }
+}
