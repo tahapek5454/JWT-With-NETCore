bc5927c41f3b64c536b68b097d51cfa91be00c13
tahapek5454 <tahapek5454@gmail.com>
2023-07-22 12:17:22 +0300

    feat: add UserController

---
+++ AuthServer.API/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : CustomBaseController
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+            var result = await _userService.CreateUserAsync(createUserDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [Authorize] //exactly want token
+        [HttpGet]
+        public async Task<IActionResult> GetUserByName()
+        {
+            // httpContext finde username from User claims
+            var result = await _userService.GetUserByUserName(HttpContext.User.Identity.Name);
+            return ActionResultInstance(result);
+        }
+    }
+}
