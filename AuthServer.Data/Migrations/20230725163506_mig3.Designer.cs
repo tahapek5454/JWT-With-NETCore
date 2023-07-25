68ccfcbd53bd297b989ff8e8cead7c309442709d
tahapek5454 <tahapek5454@gmail.com>
2023-07-25 20:46:25 +0300

    feat: add mig

--- AuthServer.Data/Migrations/20230725163506_mig3.Designer.cs
+++ AuthServer.Data/Migrations/20230725163506_mig3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AuthServer.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AuthServer.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230725163506_mig3")]
+    partial class mig3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AuthServer.Data/Migrations/20230725163506_mig3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthServer.Data.Migrations
+{
+    public partial class mig3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "AspNetUsers");
+        }
+    }
+}
